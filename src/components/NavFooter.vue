9221b63a4b32ccf8345b5b071197c80a5ddb007f
tong <2317216022@qq.com>
2020-04-07 21:26:02 +0800

    尾部组件优化

--- src/components/NavFooter.vue
+++ src/components/NavFooter.vue
@@ -5,10 +5,10 @@
       <p>小米商城</p>
     </div>             
     <div class="footer-link">
-      <a href="https://www.imooc.com/u/1343480" target="_blank">河畔一角主页</a><span>|</span>
-      <a href="https://coding.imooc.com/class/113.html" target="_blank">Vue全栈课程</a><span>|</span>
-      <a href="https://coding.imooc.com/class/236.html" target="_blank">React全家桶课程</a><span>|</span>
-      <a href="https://coding.imooc.com/class/343.html" target="_blank">微信支付专项课程（H5+小程序+Node+Mongo）</a>
+      <a href="javascript:;" target="_blank">主页</a><span>|</span>
+      <a href="javascript:;" target="_blank">博客首页</a><span>|</span>
+      <a href="javascript:;" target="_blank">码云首页</a><span>|</span>
+      <a href="javascript:;" target="_blank">高德后台管理系统</a>
     </div>
     <div class="copyright">Copyright ©2019 <span class="domain">mi.futurefe.com</span> All Rights Reserved.</div>
   </div>
